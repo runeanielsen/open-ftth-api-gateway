a47e38cab81ae9f92e139f47f6803b4bb093dd03
Jesper Ladegaard <jesper@dax.dk>
2021-02-28 15:26:12 +0100

    fix command result GraphQL field types

--- OpenFTTH.APIGateway/GraphQL/Core/Types/CommandResultType.cs
+++ OpenFTTH.APIGateway/GraphQL/Core/Types/CommandResultType.cs
@@ -10,9 +10,9 @@ namespace OpenFTTH.APIGateway.CoreTypes
         {
             Description = "Error code and message";
 
-            Field(x => x.IsSuccess, type: typeof(IdGraphType)).Name("IsSuccess").Description("True if the command was successfully processed on the server side.");
-            Field(x => x.ErrorCode, type: typeof(IdGraphType)).Name("ErrorCode").Description("Error code. Please see error code lists defined in the underlying services called");
-            Field(x => x.ErrorMessage, type: typeof(IdGraphType)).Name("ErrorMessage").Description("Error message");
+            Field(x => x.IsSuccess, type: typeof(BooleanGraphType)).Name("IsSuccess").Description("True if the command was successfully processed on the server side.");
+            Field(x => x.ErrorCode, type: typeof(StringGraphType)).Name("ErrorCode").Description("Error code. Please see error code lists defined in the underlying services called");
+            Field(x => x.ErrorMessage, type: typeof(StringGraphType)).Name("ErrorMessage").Description("Error message");
         }
     }
 }
