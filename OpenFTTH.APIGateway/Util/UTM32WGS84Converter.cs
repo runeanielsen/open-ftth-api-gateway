7805267cad7459b1adcd5d53a2d96501d10f0063
Jesper Ladegaard <jesper@dax.dk>
2022-01-06 16:33:11 +0100

    updated to latest schematic nuget with single conduit drawing fix

--- OpenFTTH.APIGateway/Util/UTM32WGS84Converter.cs
+++ OpenFTTH.APIGateway/Util/UTM32WGS84Converter.cs
@@ -36,6 +36,9 @@ namespace OpenFTTH.APIGateway.Util
 
             foreach (var inputGeoJson in geoJsonLineStrings)
             {
+                if (inputGeoJson.)
+
+
                 var line = geoJsonReader.Read<LineString>("{ \"type\": \"LineString\",\"coordinates\":" + inputGeoJson + " }");
 
                 foreach (var coord in line.Coordinates)
