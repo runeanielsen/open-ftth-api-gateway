9df0574df3998d86b354e6320ea7cecad69f89c7
Jesper Ladegaard <jesper@dax.dk>
2022-02-04 14:44:48 +0100

    fix bug in subscription registration

--- OpenFTTH.APIGateway/GraphQL/Root/OpenFTTHSubscriptions.cs
+++ OpenFTTH.APIGateway/GraphQL/Root/OpenFTTHSubscriptions.cs
@@ -17,7 +17,6 @@ namespace OpenFTTH.APIGateway.GraphQL.Root
 
             routeNetworkEventSubscription.AddFields(this);
             schematicUpdatedSubscription.AddFields(this);
-            schematicUpdatedSubscription.AddFields(this);
             terminalEquipmentConnectivityUpdatedSubscription.AddFields(this);
         }
     }
