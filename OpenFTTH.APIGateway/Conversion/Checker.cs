9ab53870ceb078711e547f54b0459fdce6524309
Jesper Ladegaard <jesper@dax.dk>
2022-02-15 10:26:04 +0100

    conversion checker

--- OpenFTTH.APIGateway/Conversion/Checker.cs
+++ OpenFTTH.APIGateway/Conversion/Checker.cs
@@ -1,4 +1,5 @@
-﻿using FluentResults;
+﻿using DAX.ObjectVersioning.Graph;
+using FluentResults;
 using Microsoft.Extensions.Logging;
 using Npgsql;
 using OpenFTTH.APIGateway.Settings;
@@ -6,20 +7,24 @@ using OpenFTTH.CQRS;
 using OpenFTTH.EventSourcing;
 using OpenFTTH.RouteNetwork.API.Model;
 using OpenFTTH.RouteNetwork.API.Queries;
+using OpenFTTH.Util;
 using OpenFTTH.UtilityGraphService.API.Commands;
 using OpenFTTH.UtilityGraphService.API.Model.UtilityNetwork;
 using OpenFTTH.UtilityGraphService.API.Queries;
 using OpenFTTH.UtilityGraphService.Business.Graph;
 using OpenFTTH.UtilityGraphService.Business.NodeContainers.Projections;
+using OpenFTTH.UtilityGraphService.Business.TerminalEquipments;
+using OpenFTTH.UtilityGraphService.Business.TerminalEquipments.Projections;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace OpenFTTH.APIGateway.Conversion
 {
     public class Checker : ImporterBase
     {
-        private ILogger<RackImporter> _logger;
+        private ILogger<Checker> _logger;
         private Guid _workTaskId;
         private IEventStore _eventStore;
         private ICommandDispatcher _commandDispatcher;
@@ -27,7 +32,7 @@ namespace OpenFTTH.APIGateway.Conversion
         private UtilityNetworkProjection _utilityNetwork;
 
 
-        public Checker(ILogger<RackImporter> logger, Guid workTaskId, IEventStore eventSTore, GeoDatabaseSetting geoDatabaseSettings, ICommandDispatcher commandDispatcher, IQueryDispatcher queryDispatcher) : base(geoDatabaseSettings)
+        public Checker(ILogger<Checker> logger, Guid workTaskId, IEventStore eventSTore, GeoDatabaseSetting geoDatabaseSettings, ICommandDispatcher commandDispatcher, IQueryDispatcher queryDispatcher) : base(geoDatabaseSettings)
         {
             _logger = logger;
             _workTaskId = workTaskId;
@@ -43,10 +48,69 @@ namespace OpenFTTH.APIGateway.Conversion
         {
             _logger.LogInformation("Checker started...");
 
-            //var terminalEquipments = _utilityNetwork.
+            var terminalEquipmentSpecifications = _eventStore.Projections.Get<TerminalEquipmentSpecificationsProjection>().Specifications;
 
+            var terminalEquipments = _utilityNetwork.TerminalEquipmentByEquipmentId.Values;
+
+            using StreamWriter csvFile = new("c:/temp/openftth_kunde_check.csv");
+
+            var csvHeader = "\"instnr\";\"rammer_odf\"";
+            csvFile.WriteLine(csvHeader);
+
+            foreach (var terminalEquipment in terminalEquipments)
+            {
+                var spec = terminalEquipmentSpecifications[terminalEquipment.SpecificationId];
+
+                if (spec.Name == "Kundeterminering")
+                {
+                    // trace terminal one
+
+                    var traceResult = _utilityNetwork.Graph.Trace(terminalEquipment.TerminalStructures[0].Terminals[0].Id);
+
+                    string endsInRack = "nej";
+
+                    if (CheckIfEndTerminalIsWithinRackEquipment(terminalEquipmentSpecifications, traceResult.Upstream))
+                        endsInRack = "ja";
+                    else if (CheckIfEndTerminalIsWithinRackEquipment(terminalEquipmentSpecifications, traceResult.Downstream))
+                        endsInRack = "ja";
+
+
+
+                    var csvLine = "\"" + terminalEquipment.Name + "\";\"" + endsInRack + "\"";
+
+                    csvFile.WriteLine(csvLine);
+                }
+            }
+
+            csvFile.Close();
 
             _logger.LogInformation("Checker finish!");
+        }
+
+        private bool CheckIfEndTerminalIsWithinRackEquipment(LookupCollection<TerminalEquipmentSpecification> terminalEquipmentSpecifications, IGraphObject[] graphObjects)
+        {
+            if (graphObjects.Length > 0)
+            {
+                var upstreamTerminal = graphObjects.Last();
+
+                if (upstreamTerminal != null)
+                {
+                    if (_utilityNetwork.Graph.TryGetGraphElement<IUtilityGraphTerminalRef>(upstreamTerminal.Id, out var terminalRef))
+                    {
+                        if (!terminalRef.IsDummyEnd)
+                        {
+                            var terminalEquipment = terminalRef.TerminalEquipment(_utilityNetwork);
+
+                            var spec = terminalEquipmentSpecifications[terminalEquipment.SpecificationId];
+
+                            if (spec.IsRackEquipment)
+                                return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
         }
 
      
--- OpenFTTH.APIGateway/Conversion/ConversionRunner.cs
+++ OpenFTTH.APIGateway/Conversion/ConversionRunner.cs
@@ -56,12 +56,15 @@ namespace OpenFTTH.APIGateway.Conversion
                 var dbToReadConversionDataFrom = _geoDatabaseSetting;
 
 
+                //new Checker(_loggerFactory.CreateLogger<Checker>(), _workTaskId, _eventStore, dbToReadConversionDataFrom, _commandDispatcher, _queryDispatcher).Run();
+
+                return;
+
                 new CableSpanEquipmentImporter(_loggerFactory.CreateLogger<ConduitSpanEquipmentImporter>(), _eventStore, dbToReadConversionDataFrom, _commandDispatcher, _queryDispatcher).Run();
 
                 new NodeContainerImporter(_loggerFactory.CreateLogger<NodeContainerImporter>(), _workTaskId, _eventStore, dbToReadConversionDataFrom, _commandDispatcher, _queryDispatcher).Run();
 
-                new RackImporter(_loggerFactory.CreateLogger<RackImporter>(), _workTaskId, _eventStore, dbToReadConversionDataFrom, _commandDispatcher, _queryDispatcher).Run();
-
+               
                 new TerminalEquipmentImporter(_loggerFactory.CreateLogger<TerminalEquipmentImporter>(), _workTaskId, _eventStore, dbToReadConversionDataFrom, _commandDispatcher, _queryDispatcher).Run();
 
                 new SegmentToTerminalConnectionsImporter(_loggerFactory.CreateLogger<SegmentToTerminalConnectionsImporter>(), _workTaskId, _eventStore, dbToReadConversionDataFrom, _commandDispatcher, _queryDispatcher).Run();
