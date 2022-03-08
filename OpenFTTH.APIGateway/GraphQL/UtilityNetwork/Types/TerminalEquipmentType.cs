82a31a87108d7e550720953e9f5c0ffca3bcc622
Rune Nielsen <runenielsen@runbox.com>
2022-03-08 11:41:15 +0100

    fixes issue with wrong specification typed returned (#22)

--- OpenFTTH.APIGateway/GraphQL/UtilityNetwork/Types/TerminalEquipmentType.cs
+++ OpenFTTH.APIGateway/GraphQL/UtilityNetwork/Types/TerminalEquipmentType.cs
@@ -1,16 +1,11 @@
 ﻿using FluentResults;
 using GraphQL.Types;
 using Microsoft.Extensions.Logging;
-using OpenFTTH.APIGateway.GraphQL.Addresses.Types;
 using OpenFTTH.CQRS;
-using OpenFTTH.RouteNetwork.API.Model;
-using OpenFTTH.RouteNetwork.API.Queries;
 using OpenFTTH.Util;
 using OpenFTTH.UtilityGraphService.API.Model.UtilityNetwork;
 using OpenFTTH.UtilityGraphService.API.Queries;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace OpenFTTH.APIGateway.GraphQL.UtilityNetwork.Types
 {
@@ -22,16 +17,16 @@ namespace OpenFTTH.APIGateway.GraphQL.UtilityNetwork.Types
             Field(x => x.Name, type: typeof(StringGraphType)).Description("Short name");
             Field(x => x.Description, type: typeof(StringGraphType)).Description("Long description");
             Field(x => x.AddressInfo, type: typeof(AddressInfoType)).Description("Address information such as access and unit address id");
-
             Field(x => x.SpecificationId, type: typeof(IdGraphType)).Description("Terminal equipment specification id");
             Field(x => x.ManufacturerId, type: typeof(IdGraphType)).Description("Terminal equipment manufacturer id");
 
-            Field<SpanEquipmentSpecificationType>(
+            Field<TerminalEquipmentSpecificationType>(
                name: "specification",
-               description: "The specification used to create the span equipment",
+               description: "The specification used to create the terminal equipment",
                resolve: context =>
                {
-                   var queryResult = queryDispatcher.HandleAsync<GetSpanEquipmentSpecifications, Result<LookupCollection<SpanEquipmentSpecification>>>(new GetSpanEquipmentSpecifications()).Result;
+                   var queryResult = queryDispatcher.HandleAsync<GetTerminalEquipmentSpecifications, Result<LookupCollection<TerminalEquipmentSpecification>>>(
+                       new GetTerminalEquipmentSpecifications()).Result;
 
                    return queryResult.Value[context.Source.SpecificationId];
                }
@@ -39,7 +34,7 @@ namespace OpenFTTH.APIGateway.GraphQL.UtilityNetwork.Types
 
             Field<ManufacturerType>(
                 name: "manufacturer",
-                description: "The manufacturer of the span equipment",
+                description: "The manufacturer of the terminal equipment",
                 resolve: context =>
                 {
                     if (context.Source.ManufacturerId == null || context.Source.ManufacturerId == Guid.Empty)
@@ -50,7 +45,6 @@ namespace OpenFTTH.APIGateway.GraphQL.UtilityNetwork.Types
                     return queryResult.Value[context.Source.ManufacturerId.Value];
                 }
             );
-
         }
     }
 }
