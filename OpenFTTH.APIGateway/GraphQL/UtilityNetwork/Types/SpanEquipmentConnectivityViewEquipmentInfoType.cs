16371a48691d53bc2b5857a5c31e614f916cedf2
Jesper Ladegaard <jesper@dax.dk>
2022-03-18 09:49:48 +0100

    add spanEquipment field to SpanEquipmentConnectivityViewEquipmentInfoType graphQL type

--- OpenFTTH.APIGateway/GraphQL/UtilityNetwork/Types/SpanEquipmentConnectivityViewEquipmentInfoType.cs
+++ OpenFTTH.APIGateway/GraphQL/UtilityNetwork/Types/SpanEquipmentConnectivityViewEquipmentInfoType.cs
@@ -1,12 +1,15 @@
 ﻿using GraphQL.Types;
 using Microsoft.Extensions.Logging;
+using OpenFTTH.CQRS;
+using OpenFTTH.UtilityGraphService.API.Model.UtilityNetwork;
 using OpenFTTH.UtilityGraphService.API.Model.UtilityNetwork.Views;
+using OpenFTTH.UtilityGraphService.API.Queries;
 
 namespace OpenFTTH.APIGateway.GraphQL.UtilityNetwork.Types
 {
     public class SpanEquipmentConnectivityViewEquipmentInfoType : ObjectGraphType<SpanEquipmentAZConnectivityViewEquipmentInfo>
     {
-        public SpanEquipmentConnectivityViewEquipmentInfoType(ILogger<SpanEquipmentConnectivityViewEquipmentInfoType> logger)
+        public SpanEquipmentConnectivityViewEquipmentInfoType(ILogger<SpanEquipmentConnectivityViewEquipmentInfoType> logger, IQueryDispatcher queryDispatcher)
         {
             Field(x => x.Id, type: typeof(IdGraphType)).Description("Master Resource Identifier UUID Property");
             Field(x => x.Name, type: typeof(StringGraphType)).Description("Name of the node structure");
@@ -14,6 +17,28 @@ namespace OpenFTTH.APIGateway.GraphQL.UtilityNetwork.Types
             Field(x => x.Info, type: typeof(StringGraphType)).Description("Additional information (remark)");
             Field(x => x.SpecName, type: typeof(StringGraphType)).Description("Specification name");
             Field(x => x.Lines, type: typeof(ListGraphType<SpanEquipmentAZConnectivityViewLineInfoType>)).Description("Connectivity lines");
+
+            FieldAsync<SpanEquipmentType>(
+              name: "spanEquipment",
+              description: "The span equipment",
+              resolve: async context =>
+              {
+                  var spanEquipmentId = context.Source.Id;
+
+                  // Get equipment information
+                  var equipmentQueryResult = await queryDispatcher.HandleAsync<GetEquipmentDetails, FluentResults.Result<GetEquipmentDetailsResult>>(
+                      new GetEquipmentDetails(new EquipmentIdList() { spanEquipmentId })
+                  );
+
+                  if (equipmentQueryResult.IsSuccess || equipmentQueryResult.Value.SpanEquipment != null && equipmentQueryResult.Value.SpanEquipment.ContainsKey(spanEquipmentId))
+                  {
+                      return equipmentQueryResult.Value.SpanEquipment[spanEquipmentId];
+                  }
+
+                  return null;
+              }
+           );
+
         }
     }
 }
