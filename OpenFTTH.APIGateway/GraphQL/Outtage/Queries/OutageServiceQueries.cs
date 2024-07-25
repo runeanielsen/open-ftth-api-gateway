de5d31dc30541e72dc12e845f38b9d080faa312e
Jesper Ladegaard <jesper@dax.dk>
2024-07-25 10:15:48 +0200

    outageView query expanded to include optional equipmentId parameter. Test data is currently returned when equipmentId parameter is set

--- OpenFTTH.APIGateway/GraphQL/Outtage/Queries/OutageServiceQueries.cs
+++ OpenFTTH.APIGateway/GraphQL/Outtage/Queries/OutageServiceQueries.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Logging;
 using OpenFTTH.APIGateway.GraphQL.Outage.Types;
 using OpenFTTH.APIGateway.GraphQL.Work.Types;
 using OpenFTTH.CQRS;
+using OpenFTTH.UtilityGraphService.API.Commands;
 using OpenFTTH.UtilityGraphService.API.Model.Outage;
 using OpenFTTH.UtilityGraphService.API.Queries;
 using OpenFTTH.Work.API.Model;
@@ -45,11 +46,40 @@ namespace OpenFTTH.APIGateway.GraphQL.Outage.Queries
                 name: "outageView",
                 description: "Information needed to show outage tree to the user",
                 arguments: new QueryArguments(
-                    new QueryArgument<NonNullGraphType<IdGraphType>> { Name = "routeNetworkElementId" }
+                    new QueryArgument<NonNullGraphType<IdGraphType>> { Name = "routeNetworkElementId" },
+                    new QueryArgument<IdGraphType> { Name = "equipmentId" }
                 ),
                 resolve: async context =>
                 {
                     var routeNetworkElementId = context.GetArgument<Guid>("routeNetworkElementId");
+
+                    if (context.HasArgument("equipmentId"))
+                    {
+                        var equipmentId = context.GetArgument<Guid>("equipmentId");
+
+                        // TODO: Replace test data with call to utility network query
+                        var rootNode = new OutageViewNode(Guid.NewGuid(), "root node", "root node eq: " + equipmentId.ToString());
+
+                        // leaf 1
+                        var leaf1 = new OutageViewNode(Guid.NewGuid(), "leaf 1 (expanded)");
+                        leaf1.Expanded = true;
+                        rootNode.AddNode(leaf1);
+
+                        leaf1.AddNode(new OutageViewNode(Guid.NewGuid(), "child 1"));
+                        leaf1.AddNode(new OutageViewNode(Guid.NewGuid(), "child 2"));
+
+
+                        // leaf 2
+                        var leaf2 = new OutageViewNode(Guid.NewGuid(), "leaf 1 (not expanded)");
+                        rootNode.AddNode(leaf2);
+
+                        leaf2.AddNode(new OutageViewNode(Guid.NewGuid(), "child 1"));
+                        leaf2.AddNode(new OutageViewNode(Guid.NewGuid(), "child 2"));
+
+                        return rootNode;
+
+                    }
+
 
                     var getOutageViewQuery = new GetOutageView(routeNetworkElementId);
 
